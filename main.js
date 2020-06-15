c8de8d8af6e5f0b5653d7b45fc67282c860abda0
tima <tima_3015@mail.ru>
2020-06-15 03:49:28 +0300

    доделать

--- main.js
+++ main.js
@@ -7,33 +7,42 @@ let getRandomNumder = (min, max) =>
 function startGame() {
   let randomNum = getRandomNumder(1, 100);
   console.log(randomNum);
+  let counter = 2;
 
   return function game() {
     let user = prompt("Введите число от 1 до 100");
     let num;
     console.log(`загаданное число ${randomNum}`);
 
-    if (isNumber(user)) {
-      num = +user;
-      if (user > randomNum) {
-        alert("Загаданное число меньше");
-        return game();
-      } else if (user < randomNum) {
-        alert("Загаданное число больше");
-        return game();
-      } else {
-        alert("Вы выиграли ");
-        if (confirm("Хочешь ещё?")) {
-          return startGame();
+    if(counter > 0){
+      if (isNumber(user)) {
+        num = +user;
+        if (user > randomNum) {
+          alert("Загаданное число меньше");
+          counter--;
+          console.log('counter: ', counter);
+          return game();
+        } else if (user < randomNum) {
+          alert("Загаданное число больше");
+          counter--;
+          console.log('counter: ', counter);
+          return game();
+        }else if(counter < 0 ) {
+          alert('game over ');
         } else {
-          alert("Пока");
+          alert("Вы выиграли ");
+          if (confirm("Хочешь ещё?")) {
+            return startGame();
+          } else {
+            alert("Пока");
+          }
         }
+      } else if (user === null) {
+        alert("До встречи");
+      } else {
+        alert("Введи число");
+        return game();
       }
-    } else if (user === null) {
-      alert("До встречи");
-    } else {
-      alert("Введи число");
-      return game();
     }
   };
 }
